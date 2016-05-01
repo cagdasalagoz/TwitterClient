c999ed81d53d06bcb26ee416b44522982bd2bb7a
cagdasalagoz <cagdasalagoz@gmail.com>
2016-05-01 18:31:06 +0300

    Can login via oauth.

--- app/src/main/java/com/twicli/torak/twitterclient/MainActivity.java
+++ app/src/main/java/com/twicli/torak/twitterclient/MainActivity.java
@@ -1,13 +1,196 @@
 package com.twicli.torak.twitterclient;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import twitter4j.Twitter;
+import twitter4j.auth.RequestToken;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
+import twitter4j.auth.AccessToken;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Constants
+    /**
+     * Register your here app https://dev.twitter.com/apps/new and get your
+     * consumer key and secret
+     * */
+    static String TWITTER_CONSUMER_KEY = "FzQviQhyhltW4yGgsOLMydzsw";
+    static String TWITTER_CONSUMER_SECRET = "w6KP7HqbakqqFnF32jwbI9RT6BAMmgUikDahwbIaXmYPvOtSbd";
+
+    // Preference Constants
+    static String PREFERENCE_NAME = "twitter_oauth";
+    static final String PREF_KEY_OAUTH_TOKEN = "oauth_token";
+    static final String PREF_KEY_OAUTH_SECRET = "oauth_token_secret";
+    static final String PREF_KEY_TWITTER_LOGIN = "isTwitterLogedIn";
+
+    static final String TWITTER_CALLBACK_URL = "https://www.twitter.com/";
+
+    // Twitter oauth urls
+    static final String URL_TWITTER_AUTH = " https://api.twitter.com/oauth/authorize";
+    static final String URL_TWITTER_OAUTH_VERIFIER = "oauth_verifier";
+    static final String URL_TWITTER_OAUTH_TOKEN = " https://api.twitter.com/oauth/access_token";
+
+    Button btnLogin;
+
+    private static Twitter twitter;
+    private  static RequestToken requestToken;
+
+    // Shared Preferences
+    private static SharedPreferences mSharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Check if twitter keys are set
+        if(TWITTER_CONSUMER_KEY.trim().length() == 0 || TWITTER_CONSUMER_SECRET.trim().length() == 0){
+            // Internet Connection is not present
+            Toast.makeText(getApplicationContext(),"Twitter oAuth tokens," +
+                    " Please set your twitter oauth tokens first!",Toast.LENGTH_SHORT).show();
+            // stop executing code by return
+            return;
+        }
+
+        btnLogin = (Button) findViewById(R.id.login_btn);
+
+        mSharedPreferences = getApplicationContext().getSharedPreferences("TwitPref",0);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // Call login twitter function
+                AsyncCallWS task = new AsyncCallWS();
+                task.execute();
+            }
+        });
+
+        /** This if conditions is tested once is
+         * redirected from twitter page. Parse the uri to get oAuth
+         * Verifier
+         * */
+        if (!isTwitterLoggedInAlready()) {
+            Uri uri = getIntent().getData();
+            if (uri != null && uri.toString().startsWith(TWITTER_CALLBACK_URL)) {
+                // oAuth verifier
+                String verifier = uri
+                        .getQueryParameter(URL_TWITTER_OAUTH_VERIFIER);
+
+                try {
+                    // Get the access token
+                    AccessToken accessToken = twitter.getOAuthAccessToken(
+                            requestToken, verifier);
+
+                    // Shared Preferences
+                    SharedPreferences.Editor e = mSharedPreferences.edit();
+
+                    // After getting access token, access token secret
+                    // store them in application preferences
+                    e.putString(PREF_KEY_OAUTH_TOKEN, accessToken.getToken());
+                    e.putString(PREF_KEY_OAUTH_SECRET,
+                            accessToken.getTokenSecret());
+                    // Store login status - true
+                    e.putBoolean(PREF_KEY_TWITTER_LOGIN, true);
+                    e.commit(); // save changes
+
+                    Log.e("Twitter OAuth Token", "> " + accessToken.getToken());
+
+                    // Hide login button
+                    btnLogin.setVisibility(View.GONE);
+
+
+
+                    // Getting user details from twitter
+                    // For now i am getting his name only
+                    long userID = accessToken.getUserId();
+                    User user = twitter.showUser(userID);
+                    String username = user.getName();
+                    Log.e("torak","welcome" + username);
+                } catch (Exception e) {
+                    // Check log for login errors
+                    Log.e("Twitter Login Error", "> " + e.getMessage());
+                }
+            }
+        }
+
     }
+
+    private void loginToTwitter() {
+        // Check if already logged in
+        if (!isTwitterLoggedInAlready()) {
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.setOAuthConsumerKey(TWITTER_CONSUMER_KEY);
+            builder.setOAuthConsumerSecret(TWITTER_CONSUMER_SECRET);
+            Configuration configuration = builder.build();
+
+            TwitterFactory factory = new TwitterFactory(configuration);
+            twitter = factory.getInstance();
+
+            try {
+                requestToken = twitter
+                        .getOAuthRequestToken(TWITTER_CALLBACK_URL);
+                this.startActivity(new Intent(Intent.ACTION_VIEW, Uri
+                        .parse(requestToken.getAuthenticationURL())));
+            } catch (TwitterException e) {
+                e.printStackTrace();
+            }
+        } else {
+            // user already logged into twitter
+            Toast.makeText(getApplicationContext(),
+                    "Already Logged into twitter", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    private class AsyncCallWS extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.i("async", "doInBackground");
+           loginToTwitter();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            Log.i("async", "onPostExecute");
+        }
+
+        @Override
+        protected void onPreExecute() {
+            Log.i("async", "onPreExecute");
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            Log.i("async", "onProgressUpdate");
+        }
+
+    }
+
+    /**
+     * Check user already logged in your application using twitter Login flag is
+     * fetched from Shared Preferences
+     * */
+    private boolean isTwitterLoggedInAlready() {
+        // return twitter login status from Shared Preferences
+        return mSharedPreferences.getBoolean(PREF_KEY_TWITTER_LOGIN, false);
+    }
+
+
 }
+
